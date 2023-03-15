dd985f630ce657b41ad0dabff666c8635284d4d4
Paul <paul.ngugi@student.moringaschool.com>
2023-03-15 18:52:09 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    # validate :custom_validator
+
+    # def custom_validator
+    #     unless title.match?("Won't Believe")
+    #         errors.add(:title, "You Won't Believe These True Facts")
+    # end
 end
